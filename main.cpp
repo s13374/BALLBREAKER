b459723d9a7976d2821d3f3d95f2837087ae61f2
Jakub Markuszewicz <jmarkuszewicz@gmail.com>
2018-01-22 18:28:01 +0100

    dodanie kolizji z podstawka

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ int bkw = 800;
 int bkh = 600;
 int bkwmin = 0;
 int bkhmin = 0;
+int cartx = bkw / 2;
+int carty = bkh-30;
 
 void EventHandler() {
 
@@ -18,7 +20,14 @@ void EventHandler() {
 	if (event.type == SDL_QUIT) {
 		quit = true;
 	}
-
+	else if (event.type == SDL_KEYDOWN) {
+		if (event.key.keysym.sym == SDLK_LEFT && cartx > 0) {
+			cartx = cartx - 2;
+		}
+		if (event.key.keysym.sym == SDLK_RIGHT && cartx < bkw - 60) {
+			cartx = cartx + 2;
+		}
+	}
 }
 
 void moveBall() {
@@ -33,6 +42,10 @@ void ball_collision() {
 	if (bally < bkhmin || bally >bkh - 30) {
 		ballvely = -ballvely;
 	}
+	int ballscaling = 20;
+	if (bally + ballscaling >= carty && bally + ballscaling <= carty + 30 && ballx + ballscaling >= cartx && ballx + ballscaling <= cartx + 60) {
+		ballvely = -ballvely;
+	}
 }
 
 int main(int argc, char ** argv) {
@@ -43,20 +56,26 @@ int main(int argc, char ** argv) {
 	SDL_Renderer *renderer = SDL_CreateRenderer(window,-1,0);
 	
 	SDL_Rect bkrect = { 0,0,800,600 };
+	
 	SDL_Surface *ball = SDL_LoadBMP("ball.bmp");
 	SDL_Surface *bk = SDL_LoadBMP("bk.bmp");
+	SDL_Surface *cart = SDL_LoadBMP("cart.bmp");
 	SDL_Texture *balltexture = SDL_CreateTextureFromSurface(renderer, ball);
 	SDL_Texture *bktexture = SDL_CreateTextureFromSurface(renderer, bk);
+	SDL_Texture *carttexture = SDL_CreateTextureFromSurface(renderer, cart);
+
 	SDL_RenderCopy(renderer, bktexture, NULL, &bkrect);
 	
 
 	while (!quit) {
 		EventHandler();
 		SDL_Rect ballrect = { ballx,bally,20,30 };
+		SDL_Rect cartrect = { cartx,carty,60,30 };
 		moveBall();
 		ball_collision();
 		SDL_RenderCopy(renderer, bktexture, NULL, &bkrect);
 		SDL_RenderCopy(renderer, balltexture, NULL, &ballrect);
+		SDL_RenderCopy(renderer, carttexture, NULL, &cartrect);
 		SDL_RenderPresent(renderer);
 		SDL_RenderClear(renderer);
 	}
